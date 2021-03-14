5cdf1abe78b31604506f34f94c21a5f1a0c26b51
Michael Taylor <patternizer@gmail.com>
2021-03-14 17:37:41 +0000

    Repo rename. Updated README.md. Added all processing chain scripts to SLURM/

--- calc_mean_20CRv3_t2m_all_years.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SBATCH --partition=short-serial 
-SBATCH --job-name=20CRv3-t2m-all-years
-SBATCH -o %j.out 
-SBATCH -e %j.err 
-SBATCH --time=30:00
-
-module load jaspy
-python calc_mean_20CRv3_t2m_all_years.py
